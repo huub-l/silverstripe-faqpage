96b920f1d8f101a05a20a5c18f6479005065a71a
Eric Hes <eric@hestec.nl>
2020-04-26 21:15:24 +0200

    workaround for preg_replace, sort

--- src/Controllers/FaqPageController.php
+++ src/Controllers/FaqPageController.php
@@ -10,12 +10,19 @@ class FaqPageController extends \PageController {
 
         $questions = array();
 
-        foreach ($this->Categories() as $cat){
+        foreach ($this->Categories()->sort('Sort') as $cat){
 
-            foreach (FaqQuestion::get()->filter('CategoryID', $cat->ID) as $q){
+            foreach (FaqQuestion::get()->filter('CategoryID', $cat->ID)->sort('Sort') as $q){
 
                 $a = Schema::answer();
-                $a->text(preg_replace('/\[(\w+)[^\]]*]([^\[]+\[\\?\/\1\])?/', '', strip_tags($q->Answer)));
+
+                $answer = preg_replace('/\[(\w+)[^\]]*]([^\[]+\[\\?\/\1\])?/', '', strip_tags($q->Answer));
+
+                /* The above regex should remove the whole embed inserts, see: https://regex101.com/r/Eaj73B/8)
+                But in php7.2 it doesn't for some unknow reason, see also: https://bit.ly/2KD5Ipa (stackoverflow)
+                That's we we remove the closing embed here with str_replace. But this doesn't remove the url between the tags. */
+
+                $a->text(str_replace('[/embed]', '', $answer));
 
                 $qa = Schema::question();
                 $qa->name($q->Question);
