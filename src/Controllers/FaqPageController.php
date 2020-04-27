90bd0e351bdfd6de14f184b5030e75e0b9baf038
Eric Hes <eric@hestec.nl>
2020-04-27 11:32:02 +0200

    updated regex

--- src/Controllers/FaqPageController.php
+++ src/Controllers/FaqPageController.php
@@ -15,15 +15,7 @@ class FaqPageController extends \PageController {
             foreach (FaqQuestion::get()->filter('CategoryID', $cat->ID)->sort('Sort') as $q){
 
                 $a = Schema::answer();
-
-                $answer = preg_replace('/\[(\w+)[^\]]*]([^\[]+\[\\?\/\1\])?/', ' ', strip_tags($q->Answer));
-
-                /* The above regex should remove the whole embed inserts, see: https://regex101.com/r/Eaj73B/8)
-                But in php7.2 it doesn't for some unknow reason, see also: https://bit.ly/2KD5Ipa (stackoverflow)
-                That's we we remove the closing embed here with str_replace. But this doesn't remove the url between the tags. */
-
-                $a->text(str_replace('[/embed]', ' ', $answer));
-
+                $a->text(preg_replace('/\[\w+[^\]]*]([^\[]+\[[\\a-zA-Z\/]+\])?/', ' ', strip_tags($q->Answer)));
                 $qa = Schema::question();
                 $qa->name($q->Question);
                 $qa->acceptedAnswer($a);
